22d3518117ad280cbab612f7aa5fda86e59a3fb1
Anna Iurchenko <anna.iurchenko@universalunits.com>
2020-08-18 19:34:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <Timeline />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Timeline from './Timeline.vue'
+
+export default defineComponent({
+  components: {
+    Timeline
+  },
+  name: 'App'
+})
+</script>
---
+++ src/Timeline.vue
@@ -0,0 +1,35 @@
+<template>
+  <nav class="is-primary panel">
+    <p class="panel-tabs">
+      <a v-for="period in periods" :key="period" data-test="period"
+        :class="[ period === selectedPeriod ? 'is-active' : '']"
+        @click="setPeriod(period)"
+      >
+        {{ period }}
+      </a>
+    </p>
+  </nav>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+
+import { Period } from './types'
+
+export default defineComponent({
+  setup() {
+    const periods: Period[] = ['today', 'this week', 'this month']
+    const selectedPeriod = ref<Period>('today')
+
+    const setPeriod = (period: Period) => {
+      selectedPeriod.value = period
+    }
+
+    return {
+      periods,
+      selectedPeriod,
+      setPeriod
+    }
+  }
+})
+</script>
